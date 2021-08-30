c8e466efa61bda310c352e70761d3fd153100664
falcosan <danielefalche@gmail.com>
2021-08-30 02:16:47 +0200

    .

--- blog/getArticles.js
+++ blog/getArticles.js
@@ -1,8 +1,9 @@
 const storyblok = require('../functions/import');
-const filter = 'Storyblok webhooks'
+const path = require('path');
+const filter = ''
 const lang = 'it'
 
-module.exports.getArticles = async function (path, type = 'md') { 
+module.exports.getArticles = async function (type = 'md') { 
       const { data } = await storyblok.get('cdn/stories', {
         starts_with: 'blog/',
         filter_query: {
@@ -13,6 +14,6 @@ module.exports.getArticles = async function (path, type = 'md') {
         language: lang
       })
       const content = data.stories[0].content.long_text
-      fs.writeFileSync(`./${path}/${data.stories[0].name}.${type}`, content)
+      fs.writeFileSync(`${path.join(__dirname, '/articles')}/${data.stories[0].name}.${type}`, content)
 };
 
