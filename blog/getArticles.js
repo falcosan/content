4dc8bf439bed9a937ace25cb00f805967c715f92
falcosan <danielefalche@gmail.com>
2021-08-30 09:06:51 +0200

    .

--- blog/getArticles.js
+++ blog/getArticles.js
@@ -2,16 +2,16 @@ const storyblok = require('../utils/api');
 const path = require('path');
 
 const field = 'long_text'
-const lang = 'it'
+const lang = 'en'
 const type = 'md'
 
 module.exports.getArticles = async function () { 
       const { data } = await storyblok.get('cdn/stories', {
         starts_with: 'blog/',
-        language: lang
+        language: lang === 'en' ? '' : lang
       })
      data.stories.map(function(story) {
-        const files = story.name.toLowerCase() !== 'blog' ? fs.writeFileSync(`${path.join(__dirname, '/articles')}/${story.name}.${type}`, story.content[field]) : ''
+        const files = story.name.toLowerCase() !== 'blog' ? fs.writeFileSync(`${path.join(__dirname, `/articles/${lang}`)}/${story.name}.${type}`, story.content[field]) : ''
         return Boolean(files)
       })
 };
