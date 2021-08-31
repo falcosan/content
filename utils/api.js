68f9ef3c15083e555ad3c2bc4da8aa1eb2c127c4
falcosan <danielefalche@gmail.com>
2021-08-31 12:33:27 +0200

    k

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,7 @@
 
 const StoryblokClient = require('storyblok-js-client')
 require('dotenv').config();
+
 module.exports = storyblok = new StoryblokClient({
     accessToken: process.env.NUXT_ENV_PREVIEW_TOKEN,
     cache: {
