c03c3d5b649d43be8ed2875e763f3217b1d29044
falcosan <danielefalche@gmail.com>
2021-08-31 12:53:08 +0200

    .

--- blog/getBlog.js
+++ blog/getBlog.js
@@ -1,14 +1,16 @@
 const field = 'long_text'
 
+
 module.exports.getBlog = async function () { 
 const storyblok = require('../utils/api');
 const path = require('path');
 require('dotenv').config();
   const languages = await storyblok.get(`cdn/spaces/${process.env.STORYBLOK_ID_SPACE}`)
+  languages.data.space.language_codes.unshift('en')
   languages.data.space.language_codes.forEach( async function (lang){ 
       const { data } = await storyblok.get('cdn/stories', {
         starts_with: 'blog/',
-        language: lang
+        language: lang === 'en' ? '' : lang
       })
      data.stories.map(function(story) {
         const files = story.name.toLowerCase() !== 'blog' ? fs.writeFileSync(`${path.join(__dirname, `/articles/${lang}`)}/${story.name}.md`, story.content[field]) : ''
