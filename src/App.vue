cd68762bc59b2b14f5dc88d12b4874220a0f4d26
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-07-02 00:58:28 +0200

    a

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
-import { inject } from 'vue';
 import Navbar from '@/components/Navbar';
-const auth = inject('auth');
 </script>
 
 <template>
-    <Navbar v-if="auth" />
+    <Navbar />
     <RouterView />
 </template>
