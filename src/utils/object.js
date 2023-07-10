c54623475a6bd82145d6b37a1ed5ade60dd6afb9
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-07-10 19:17:47 +0200

    as

--- src/utils/object.js
+++ src/utils/object.js
@@ -1,22 +1,26 @@
-export const importFilter = (data, filters, rule) => {
-    if (rule) {
-        return Object.fromEntries(
-            Object.entries(data).filter(([k]) =>
-                new RegExp(
-                    Array.isArray(filters) ? filters.join('|') : filters
-                ).test(k)
-            )
-        );
-    } else {
-        return Object.fromEntries(
-            Object.entries(data).filter(
-                ([k]) =>
-                    !new RegExp(
-                        Array.isArray(filters) ? filters.join('|') : filters
-                    ).test(k)
-            )
-        );
-    }
+export const importFilter = (data, filters, rule, strict = false) => {
+    const filterFn = (k) => {
+        if (strict) {
+            if (Array.isArray(filters)) {
+                return rule
+                    ? filters.every((filter) => k === filter)
+                    : filters.every((filter) => k !== filter);
+            } else {
+                return rule ? k === filters : k !== filters;
+            }
+        } else {
+            const filterRegex = new RegExp(
+                Array.isArray(filters) ? filters.join('|') : filters
+            );
+            return rule ? filterRegex.test(k) : !filterRegex.test(k);
+        }
+    };
+    return Object.entries(data).reduce((acc, [k, v]) => {
+        if (filterFn(k)) {
+            acc[k] = v;
+        }
+        return acc;
+    }, {});
 };
 
 export const checkEqualObjects = (a, b) => {
