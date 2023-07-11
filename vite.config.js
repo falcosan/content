a3812eebe4d4883daf6c13bdcbc15b7741aa95ee
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-07-11 12:07:32 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,30 @@
-import { defineConfig } from 'vite';
+import enums from './src/enums';
+import process from 'node:process';
 import { fileURLToPath } from 'url';
 import vue from '@vitejs/plugin-vue';
-import enums from './src/enums';
 import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig, loadEnv } from 'vite';
 import { htmlPlugin } from './src/plugins/html.js';
 
-export default defineConfig({
-    plugins: [
-        vue(),
-        htmlPlugin(),
-        VitePWA({
-            manifest: enums.manifest,
-            registerType: 'autoUpdate',
-        }),
-    ],
-    envPrefix: 'STORY_',
-    resolve: {
-        extensions: ['.js', '.vue', '.json'],
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+    return {
+        plugins: [
+            vue(),
+            htmlPlugin(),
+            VitePWA({
+                manifest: enums.manifest,
+                registerType: 'autoUpdate',
+            }),
+        ],
+        define: {
+            'process.env': loadEnv(mode, process.cwd(), ''),
         },
-    },
+        envPrefix: 'STORY_',
+        resolve: {
+            extensions: ['.js', '.vue', '.json'],
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
+        },
+    };
 });
