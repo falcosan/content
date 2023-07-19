63819ab5e8300a00be922348c36109dc2ca93cbd
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-07-19 15:44:58 +0200

    a

--- src/components/Editor/Extensions/Embed/index.js
+++ src/components/Editor/Extensions/Embed/index.js
@@ -29,8 +29,9 @@ export const EmbedCustom = Node.create({
             twitchLive: /https:\/\/www\.twitch\.tv\/(.*)/,
             vimeo: /(https:\/\/)?(www\.)?vimeo\.com\/(\d+)/,
             twitchClip: /^https:\/\/(?:www\.)?twitch\.tv\/\w+\/clip\/(\w+)-(\w+)/,
-            youtube: /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+).*$/,
             dailymotion: /(https?:\/\/)(www\.)?(dailymotion\.com\/video\/)([a-zA-Z0-9]+)(.*)/,
+            youtube:
+                /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|(?:youtube\.com\/(?:watch\?.*v=|embed\/|v\/)))([a-zA-Z0-9_-]+)(?:\S*)$/,
             url: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|ftp:\/\/|ftps:\/\/|blob:|localhost)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
         }
         function convertUrl(url) {
@@ -58,7 +59,10 @@ export const EmbedCustom = Node.create({
                     'https://player.twitch.tv/?channel=$1'
                 )}&${hosts}`
             } else if (patterns.youtube.test(url)) {
-                return `https://www.youtube.com/embed/${url.replace(patterns.youtube, '$3')}`
+                const time = url.match(/(?:t|start)=(\d+)/)
+                let path = `https://www.youtube.com/embed/${url.replace(patterns.youtube, '$1')}`
+                if (time) path = `${path}?start=${time[1]}`
+                return path
             } else {
                 return url
             }
