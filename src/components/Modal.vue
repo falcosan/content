047fdbd293777eeeaa69c399dcf1c726eb037635
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-07-20 17:27:53 +0200

    q

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,28 +1,26 @@
 <template>
-    <div :class="['modal', { opened: open }]">
-        <transition enter-from-class="opacity-0" enter-active-class="transition">
-            <div
-                v-show="open"
-                ref="modal"
-                class="fixed h-screen w-screen flex items-center justify-center left-0 top-0 px-2.5 md:px-5 z-20 bg-opacity-40 bg-gray-900"
-                tabindex="0"
-                @click.self="closeModal"
-                @keydown.esc="closeModal"
-            >
-                <div class="w-full max-w-lg p-2.5 md:p-5 rounded bg-white">
-                    <div v-if="hasSlot('header')" class="mb-5">
-                        <slot v-if="hasSlot('header')" name="header" />
-                    </div>
-                    <div v-if="hasSlot('body')">
-                        <slot name="body" />
-                    </div>
+    <div v-if="show" :class="['modal', { opened: open }]">
+        <div
+            v-show="open"
+            ref="modal"
+            class="fixed h-screen w-screen flex items-center justify-center left-0 top-0 px-2.5 md:px-5 z-20 bg-opacity-40 bg-gray-900"
+            tabindex="0"
+            @click.self="closeModal"
+            @keydown.esc="closeModal"
+        >
+            <div class="w-full max-w-lg p-2.5 md:p-5 rounded bg-white">
+                <div v-if="hasSlot('header')" class="mb-5">
+                    <slot v-if="hasSlot('header')" name="header" />
+                </div>
+                <div v-if="hasSlot('body')">
+                    <slot name="body" />
                 </div>
             </div>
-        </transition>
+        </div>
     </div>
 </template>
 <script>
-import { ref, watch, nextTick, useSlots, onBeforeUnmount } from 'vue'
+import { ref, watch, nextTick, useSlots, onBeforeUnmount, reactive, toRefs } from 'vue'
 export default {
     props: {
         open: {
@@ -33,13 +31,21 @@ export default {
     emits: ['update:open'],
     setup(props, { emit }) {
         const modal = ref(null)
+        const state = reactive({
+            show: false,
+        })
+        const { show } = toRefs(state)
         const checkModal = () => {
             if (props.open) {
-                document.body.appendChild(modal.value)
-                nextTick(() => modal.value.focus({ preventScroll: true }))
+                show.value = true
+                nextTick(() => {
+                    document.body.appendChild(modal.value)
+                    modal.value.focus({ preventScroll: true })
+                })
             } else {
                 modal.value.parentNode.removeChild(modal.value)
                 document.querySelector('.modal.opened').appendChild(modal.value)
+                show.value = false
             }
         }
         const closeModal = () => emit('update:open', !props.open)
@@ -52,6 +58,7 @@ export default {
         })
         watch(() => props.open, checkModal)
         return {
+            show,
             modal,
             hasSlot,
             closeModal,
