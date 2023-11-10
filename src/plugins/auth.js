001838a587511ecc2700d61b098ba804ceebcaf8
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-11-10 18:24:16 +0100

    f

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -4,24 +4,39 @@ import { setCookie, getCookie, deleteCookie } from '@/utils/cookies'
 export default {
     install: (app) => {
         const auth = ref(false)
+        const path = getCookie('path')
         const db = createClient(process.env.STORY_SUPABASE_URL, process.env.STORY_SUPABASE_TOKEN)
         if (process.env.NODE_ENV === 'development') {
             auth.value = getCookie('auth') === process.env.STORY_AUTH_SECRET
+            watch(
+                auth,
+                (val) => {
+                    if (val) {
+                        if (/login/.test(window.location.pathname)) {
+                            setCookie('auth', val)
+                            window.location.replace(path ?? '/')
+                            deleteCookie('path')
+                        }
+                    } else {
+                        setCookie('path', window.location.href)
+                    }
+                },
+                { immediate: true }
+            )
         } else {
-            db.auth.onAuthStateChange((_, session) => {
-                if (session) auth.value = true
+            db.auth.onAuthStateChange((event, session) => {
+                if (session) {
+                    auth.value = true
+                    if (/login/.test(window.location.pathname)) {
+                        window.location.replace(path ?? '/')
+                        deleteCookie('path')
+                    }
+                } else if (event === 'INITIAL_SESSION') {
+                    setCookie('path', window.location.href)
+                }
             })
         }
-        if (!auth.value) setCookie('path', window.location.href)
-        watch(auth, (val) => {
-            if (val) {
-                if (/login/.test(window.location.pathname)) {
-                    if (process.env.NODE_ENV === 'development') setCookie('auth', val)
-                    window.location.replace(getCookie('path'))
-                    deleteCookie('path')
-                }
-            }
-        })
+
         app.provide('db', db)
         app.provide(
             'auth',
