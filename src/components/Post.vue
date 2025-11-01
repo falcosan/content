c60a2b57eabfcd6ff53e1906ebc948d5a7891d46
Daniele Falchetti <danielefalche@gmail.com>
2025-11-01 14:50:22 +0100

    refactor: replace Modal component with a transition for modal messages in Post.vue

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { Icon } from '@iconify/vue'
-import Modal from '@/components/Modal'
 import Editor from '@/components/Editor'
 import { dateWithoutHour } from '@/utils/date'
 import { useDetail } from '@/composables/useDetail'
@@ -18,6 +17,25 @@ const { modal, detail, inputs, editors, loading, modalType, editDetail, goToDeta
 
 <template>
     <div v-if="detail.content">
+        <Transition
+            enter-active-class="transition-all duration-300 ease-out"
+            enter-from-class="opacity-0 -translate-y-2"
+            enter-to-class="opacity-100 translate-y-0"
+            leave-active-class="transition-all duration-200 ease-in"
+            leave-from-class="opacity-100 translate-y-0"
+            leave-to-class="opacity-0 -translate-y-2"
+        >
+            <div
+                v-if="modal.state"
+                :class="[
+                    'fixed top-5 left-1/2 -translate-x-1/2 z-50 px-6 py-3 rounded-lg shadow-lg font-semibold',
+                    modalType[modal.type].background,
+                    modalType[modal.type].text,
+                ]"
+            >
+                {{ modal.message }}
+            </div>
+        </Transition>
         <div class="grid grid-cols-12 gap-x-5">
             <div
                 v-for="(input, indexInput) in inputs"
@@ -71,17 +89,5 @@ const { modal, detail, inputs, editors, loading, modalType, editDetail, goToDeta
                 <span v-else v-text="detail.published ? 'Unpublish' : 'Publish'" />
             </button>
         </div>
-        <Modal v-model:open="modal.state">
-            <template v-if="modal.type" #body>
-                <div
-                    :class="[
-                        'p-2.5 md:p-5 rounded text-center text-xl font-semibold',
-                        modalType[modal.type].background,
-                    ]"
-                >
-                    <span :class="[modalType[modal.type].text]" v-text="modal.message" />
-                </div>
-            </template>
-        </Modal>
     </div>
 </template>
