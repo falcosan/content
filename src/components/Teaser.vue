af5ed0497de72d9452b272587400cea4c323f5f3
Daniele Falchetti <danielefalche@gmail.com>
2025-11-01 14:54:22 +0100

    fix: update background color in Teaser component for better visibility

--- src/components/Teaser.vue
+++ src/components/Teaser.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
     <div v-if="data.content" class="w-full py-2 first:pt-0 last:pb-0 cursor-pointer">
-        <div class="p-3 rounded bg-neutral-400">
+        <div class="p-3 rounded bg-neutral-300">
             <p class="font-semibold text-gray-900">{{ data.content.title }}</p>
             <p class="text-gray-600 italic">{{ data.content.intro }}</p>
         </div>
